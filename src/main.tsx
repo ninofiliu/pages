cdcb7cf24d2f177a46b833ed2e96155a17c93c7e
Nino Filiu <nino.filiu@gmail.com>
2021-02-18 22:44:26 +0100

    eslint setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const App = ({n}: {n: number}) => <div>{2*n}</div>;
+const App = ({ n }: {n: number}) => <div>{2 * n}</div>;
 
 ReactDOM.render(
-  <App n={10}/>,
-  document.getElementById('root')
-)
+  <App n={10} />,
+  document.getElementById('root'),
+);
