290bee13e81aa63e5747237d4ce9fc2af692a5ee
Nino Filiu <nino.filiu@gmail.com>
2021-02-19 00:18:33 +0100

    react-router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
 import React, { useState } from 'react';
+import {
+  BrowserRouter, Switch, Route, Link, Redirect,
+} from 'react-router-dom';
 import './App.scss';
+
+const Home = () => <div>Home</div>;
+const About = () => <div>About</div>;
 
 export default () => {
   const [menuOpened, setMenuOpened] = useState<boolean>(false);
   return (
-    <div className="App">
-      <div className="content">
-        <div className="content-header">
-          <div className="title">
-            Nino Filiu
+    <BrowserRouter>
+      <div className="App">
+        <div className="content">
+          <div className="content-header">
+            <div className="title">
+              Nino Filiu
+            </div>
+            <button
+              type="button"
+              className="menu-toggle"
+              onClick={() => setMenuOpened(!menuOpened)}
+            >
+              &#x2630;
+            </button>
           </div>
-          <button
-            type="button"
-            className="menu-toggle"
-            onClick={() => setMenuOpened(!menuOpened)}
-          >
-            &#x2630;
-          </button>
+          <Switch>
+            <Route exact path="/"><Home /></Route>
+            <Route path="/about"><About /></Route>
+            <Route path="/*"><Redirect to="/" /></Route>
+          </Switch>
+        </div>
+        <div className={`menu ${menuOpened ? '--opened' : '--hidden'}`}>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
         </div>
       </div>
-      <div className={`menu ${menuOpened ? '--opened' : '--hidden'}`} />
-    </div>
+    </BrowserRouter>
   );
 };
