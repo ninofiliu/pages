f77f5065916420bd0bc562860ccae5423d68055a
Nino Filiu <nino.filiu@gmail.com>
2021-02-21 20:02:08 +0100

    /

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import React, { useState } from 'react';
 import {
   BrowserRouter, Switch, Route, Link, Redirect,
 } from 'react-router-dom';
+import Home from './views/Home';
 import './App.scss';
-
-const Home = () => <div>Home</div>;
-const About = () => <div>About</div>;
 
 export default () => {
   const [menuOpened, setMenuOpened] = useState<boolean>(false);
@@ -27,13 +25,11 @@ export default () => {
           </div>
           <Switch>
             <Route exact path="/"><Home /></Route>
-            <Route path="/about"><About /></Route>
             <Route path="/*"><Redirect to="/" /></Route>
           </Switch>
         </div>
         <div className={`menu ${menuOpened ? '--opened' : '--hidden'}`}>
           <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
         </div>
       </div>
     </BrowserRouter>
---
+++ src/views/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Home.scss';
+
+export default () => (
+  <div className="Home">
+    <p>
+      I am Nino Filiu, software engineer and artist. Welcome to my nerd crib, you&apos;ll find here some web utilities I&apos;ve made. Find me elsewhere: <a target="_blank" rel="noreferrer noopener" href="https://ninofiliu.com/">ninofiliu.com</a> / <a target="_blank" rel="noreferrer noopener" href="https://twitter.com/ninofiliu">Twitter</a> / <a target="_blank" rel="noreferrer noopener" href="https://github.com/ninofiliu/">Github</a> / <a target="_blank" rel="noreferrer noopener" href="https://www.instagram.com/ssttaacckkyy/">Instagram</a> / <a target="_blank" rel="noreferrer noopener" href="https://stackoverflow.com/users/8186898/nino-filiu">Stack Overflow</a>
+    </p>
+  </div>
+);
